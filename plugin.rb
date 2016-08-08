fd9ea9f01eb5a54b4f136a58c06d7522467749f1
netarmy <misaka4e21@gmail.com>
2016-08-08 11:11:01 +0800

    Make modifications on app/serializers/admin_user{,_list}_serializer.rb

--- plugin.rb
+++ plugin.rb
@@ -4,9 +4,25 @@
 # authors: netarmy <misaka4e21@gmail.com>
 
 after_initialize do
-  UserGuardian.module_eval do
-    def can_see_staff_info?(user)
-      user && is_admin?
+  AdminUserListSerializer.class_eval do
+    def include_email?
+      (scope.is_admin? && object.id == scope.user.id) || scope.can_see_emails?
+    end
+  end
+  AdminUserSerializer.class_eval do
+    def can_see_ip?
+      scope.is_admin? && object.id == scope.user.id
+    end
+    def ip_address
+      object.ip_address.try(:to_s) if can_see_ip? else '127.233.233.233'
+    end
+    
+    def registration_ip_address
+      if can_see_ip? then
+        object.ip_address.try(:to_s)
+      else
+        '127.233.233.233'
+      end
     end
   end
 end
