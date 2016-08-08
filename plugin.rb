8ae822876ba77c64b3ae3e4178b67321ec6c64fe
netarmy <misaka4e21@gmail.com>
2016-08-08 11:26:27 +0800

    Restrict moderator modification to core information; Keep making effort to hide email addresses from moderator's eyes.

--- plugin.rb
+++ plugin.rb
@@ -4,12 +4,46 @@
 # authors: netarmy <misaka4e21@gmail.com>
 
 after_initialize do
+
+  UserGuardian.module_eval do
+    def can_edit_user?(user)
+      is_me?(user) || is_admin?
+    end
+    def can_edit_username?(user)
+      return false if (SiteSetting.sso_overrides_username? && SiteSetting.enable_sso?)
+      return true if is_admin?
+      return false if SiteSetting.username_change_period <= 0
+      is_me?(user) && (user.post_count == 0 || user.created_at > SiteSetting.username_change_period.days.ago)
+    end
+
+    def can_edit_email?(user)
+      return false if (SiteSetting.sso_overrides_email? && SiteSetting.enable_sso?)
+      return false unless SiteSetting.email_editable?
+      return true if is_admin?
+      can_edit?(user)
+    end
+
+    def can_edit_name?(user)
+      return false if not(SiteSetting.enable_names?)
+      return false if (SiteSetting.sso_overrides_name? && SiteSetting.enable_sso?)
+      return true if is_staff?
+      can_edit?(user)
+    end
+    
+    def can_anonymize_user?(user)
+      is_admin? && !user.nil? && !user.staff?
+    end
+  end
+
   AdminUserListSerializer.class_eval do
     def include_email?
-      (scope.is_admin? && object.id == scope.user.id) || scope.can_see_emails?
+      (scope.is_admin? && object.id == scope.user.id)
     end
   end
   AdminUserSerializer.class_eval do
+    def include_email?
+      (scope.is_admin? && object.id == scope.user.id)
+    end
     def can_see_ip?
       scope.is_admin? && object.id == scope.user.id
     end
